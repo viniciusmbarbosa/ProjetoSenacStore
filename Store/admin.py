67e73f5452ecfbb710d5c3e6ddba3421da795863
Vinicius.Mirandaa <viniciusdemirandabarbosa@gmail.com>
2022-07-13 22:26:20 -0300

    Alteraçoes

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from Store.models import Categoria, Departamento, Produto
+
 # Register your models here.
+admin.site.register(Departamento)
+admin.site.register(Categoria)
+admin.site.register(Produto)
--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,31 @@
+from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=20)
+
+    def _str_(self):
+        return self.nome
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    Departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+         return self.nome
+
+# Produtos!
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=30)
+    descrição = models.TextField(max_length=8000)
+    imagem = models.ImageField(upload_to='images/')
+    preco = models.DecimalField(max_digits=10,decimal_places=2)
+
+    estoque = models.IntegerField()
+    lancamento = models.BooleanField()
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.nome
