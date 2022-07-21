884628134d7ae437119f80d663fe3553196799d2
Vinicius.Mirandaa <viniciusdemirandabarbosa@gmail.com>
2022-07-20 21:42:57 -0300

    Melhorias no admin

--- Store/admin.py
+++ Store/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-
+from django.utils.html import mark_safe
 from Store.models import Categoria, Departamento, Produto
 
 # Register your models here.
-admin.site.register(Departamento)
-admin.site.register(Categoria)
-admin.site.register(Produto)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['id','nome']
+    list_display_link = ['id','nome']
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['id','nome','Departamento']
+    list_display_link = ['id','nome']   
+    list_filter = ['Departamento']
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['id','nome','preco','estoque','categoria','ver_imagem']
+    list_display_link = ['id','nome']   
+    list_filter = ['categoria']
+    search_fields = ['nome']
+    readonly_fields = ['ver_imagem']
+
+    def ver_imagem(self,obj):
+        return mark_safe('<img src="{url}"width="{width}" heigth="{height}"/>'.format(url = obj.imagem.url,width=75,height=75))
+admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+ 
+
+ 
--- Store/models.py
+++ Store/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Departamento(models.Model):
     nome = models.CharField(max_length=20)
 
-    def _str_(self):
+    def __str__(self):
         return self.nome
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
